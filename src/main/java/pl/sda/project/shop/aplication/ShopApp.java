5939e8a8b0a05305dfdd50a15310de7459df335a
Grzesiek <grzesiek138@onet.eu>
2022-06-25 11:33:37 +0200

    hello world2

--- src/main/java/pl/sda/project/shop/aplication/ShopApp.java
+++ src/main/java/pl/sda/project/shop/aplication/ShopApp.java
@@ -3,5 +3,6 @@ package pl.sda.project.shop.aplication;
 public class ShopApp {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world2");
     }
 }
